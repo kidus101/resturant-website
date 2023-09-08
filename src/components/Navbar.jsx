be6116719f1843cb4306c30a30f7b737ff1de702
kidus <kidusabebe101@gmail.com>
2023-09-08 13:40:03 +0300

    Adding the Sidebar for the Desktop View

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { AiOutlineMenu ,AiOutlineSearch } from "react-icons/ai"
-import {BsFillCartFill} from "react-icons/bs"
+import React, { useState } from 'react'
+import { AiFillTag, AiOutlineClose, AiOutlineMenu ,AiOutlineSearch } from "react-icons/ai"
+import {BsFillCartFill, BsFillSaveFill} from "react-icons/bs"
+import {TbTruckDelivery} from "react-icons/tb"
+import {MdFavorite, MdHelp} from "react-icons/md"
+import {FaUserFriends} from "react-icons/fa"
+import {FaWallet} from "react-icons/fa"
+
+
 const Navbar = () => {
+
+    const [ nav , setNav ] = useState(false)
+
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4' >
         {/*  Left Column  */}
         <div className='flex items-center'>
             <div className='cursor-pointer'>
-                <AiOutlineMenu size={30}/>
+                <AiOutlineMenu 
+                    onClick={() => setNav(!nav)}
+                    size={30}/>
             </div>
 
             <h1 className='text-2xl sm:text-3xl lg:text-4xl px-4'> 
                 Gojo <span className='font-extrabold'> Foods  </span>
             </h1>
 
-            <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+            <div className='hidden lg:flex items-center bg-gray-200 rounded-full ml-6 p-2 text-[14px]'>
                 <p className='bg-black text-white rounded-full p-2'>
                     Delivery
                 </p>
@@ -30,9 +41,61 @@ const Navbar = () => {
 
         {/* Cart Button*/}
         <button className='bg-black text-white rounded-full hidden md:flex items-center py-2'>
-            <BsFillCartFill size={20} className='mr-2 '/> Cart
+            <BsFillCartFill 
+                size={20} 
+                className='mr-2 '/> Cart
         </button>
 
+        {/* Mobile Menu */}
+        {/* Oveylay */}
+        <div className={ nav ? 'bg-black/80 fixed w-full h-screen z-10 top-0 left-0' : ""}>
+
+        </div>
+
+        <div className={ nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 
+        'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300' }>
+           
+           <AiOutlineClose
+           onClick={() => setNav(!nav)}
+            size={30}
+            className='absolute right-4 top-4 cursor-pointer '
+           />
+           
+            <h2 className='text-2xl p-4'>Gojo <span className='font-extrabold'>Foods</span>  </h2>
+
+            <nav>
+                <ul className='flex flex-col p-4 text-gray-800'>
+                    <li className='text-lg py-2 flex'> 
+                        <TbTruckDelivery size={25} className='mr-4' /> Orders
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <MdFavorite size={25} className='mr-4' /> Favorites
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <FaWallet size={25} className='mr-4' /> Wallet
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <MdHelp size={25} className='mr-4' /> Help
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <AiFillTag size={25} className='mr-4' /> Promotions
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <BsFillSaveFill size={25} className='mr-4' /> Best One
+                    </li>
+
+                    <li className='text-lg py-2 flex'> 
+                        <FaUserFriends size={25} className='mr-4' /> Invite Friends
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
 
     </div>
   )
